2d4ff03b10aa6d6e76450a5367078bddc88770fc
najwasakuraui-create <najwasakuraui@gmail.com>
2025-11-06 22:15:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Dark mode toggle
-const darkToggle = document.getElementById('darkToggle');
-darkToggle.addEventListener('click', () => {
-  document.body.classList.toggle('bg-dark');
-  document.body.classList.toggle('text-light');
-  darkToggle.textContent = document.body.classList.contains('bg-dark')
-    ? 'Light Mode' : 'Dark Mode';
-});
-
 // Play/pause anthem
 const playBtn = document.getElementById('playBtn');
 const anthem = document.getElementById('anthemAudio');
